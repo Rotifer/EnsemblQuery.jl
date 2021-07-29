7d74ec51a6d28c29a82a29deff3700badc18d06f
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-07-29 16:25:02 +0100

    Update EnsemblQuery.jl
    Added a test function for querying the Ensembl API

--- src/EnsemblQuery.jl
+++ src/EnsemblQuery.jl
@@ -1,5 +1,23 @@
 module EnsemblQuery
 
-greet() = print("Hello World!")
+export getxrefsforgene
+
+function getxrefsforgene(ensembl_gene_id)
+    try
+        response = HTTP.request("GET", "https://rest.ensembl.org/xrefs/id/" * ensembl_gene_id * "?", 
+                               [
+                                 "Content_Type" => "application/json"
+                               ])
+        if resonse.status == 200
+            result = JSON.parse(String(response.body))
+            return JSON.json(result)
+        else
+            error("REST call error")
+        end
+    catch y
+        println("HTTP request failed: $y.")
+    end
+end
+
 
 end # module
