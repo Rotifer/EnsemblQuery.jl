4ed8e011df2936227f0d0781462c5294c335054c
Michael Maguire <mick@javascript-spreadsheet-programming.com>
2021-07-30 10:55:23 +0100

    Refactored REST fetch call
    Created a function to return JSON for a given end point URL. This can be used by all other functions to return JSON and centralizes the REST call for easier re-use and error handling

--- src/EnsemblQuery.jl
+++ src/EnsemblQuery.jl
@@ -6,22 +6,23 @@ export get_xrefs_for_ensembl_id
 
 base_url = "https://rest.ensembl.org/"
 
-function get_xrefs_for_ensembl_id(ensembl_id)
-    url = "($base_url)xrefs/id/$(ensembl_id)?"
-    try
-        response = HTTP.request("GET", url,
-                                [
-                                  "Content_Type" => "application/json"
-                                ])
-        if response.status == 200
-            result = JSON.parse(String(response.body))
-            return JSON.json(result)
-        else
-            error("ERROR: HTTP response $(response.status) for URL $(url)")
-        end
-    catch y
-        println("HTTP request failed: $y.")
+function get_json_for_end_point(end_point_url)
+    url = "$(base_url)$(end_point_url)
+    response = HTTP.request("GET", url,
+                            [
+                                "Content_Type" => "application/json"
+                            ])
+    if response.status == 200
+       result = JSON.parse(String(response.body))
+       return JSON.json(result)
+    else
+       error("ERROR: HTTP response $(response.status) for URL $(url)")
     end
+end
+
+function get_xrefs_for_ensembl_id(ensembl_id)
+    end_point_url = "xrefs/id/$(ensembl_id)?"
+    return get_json_for_end_point(end_point_url)
 end
 
 
